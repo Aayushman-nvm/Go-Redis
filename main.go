69ce4c857ce4fa2b700342b62a13f5a158ae7800
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-07-07 23:17:45 +0530

    tcp server

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-    fmt.Println("Hello World")
+	//Creating a tcp listener to accept tcp connection request
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Waiting for a client to connect to our listener
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		//Our buffer
+		buf := make([]byte, 1024)
+
+		//From client connection, we read <buf> bytes at a time
+		_, err := conn.Read(buf)
+		if err != nil {
+			//If we reach EOF (End of file)... good, no problem
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		//Once read successfully, we send back an "OK" message to the client
+		conn.Write([]byte("+OK\r\n"))
+	}
+
+	fmt.Println("Hello World")
 }
